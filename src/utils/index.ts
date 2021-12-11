f3a19bd15f2e36c4297382d68aaac7bacf045a0e
Oyster Lee <oysterd3@gmail.com>
2021-12-11 14:57:58 +0800

    🎉 Initial commit

---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+const heavenlyStems = [
+  "甲",
+  "乙",
+  "丙",
+  "丁",
+  "戊",
+  "已",
+  "庚",
+  "辛",
+  "壬",
+  "癸",
+];
+
+const earthlyBranches = [
+  "子",
+  "丑",
+  "寅",
+  "卯",
+  "辰",
+  "巳",
+  "午",
+  "未",
+  "申",
+  "酉",
+  "戌",
+  "亥",
+];
+
+const getYearSexagenaryCycle = () => {};
